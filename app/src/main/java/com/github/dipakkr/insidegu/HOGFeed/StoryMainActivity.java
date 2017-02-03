4c23b7e95f053187b6927345b2d8d2b21e2824c0
Deepak <raj.deeepak@gmail.com>
2017-02-03 22:53:24 +0530

    [+] added splash screen

---
+++ app/src/main/java/com/github/dipakkr/insidegu/HOGFeed/StoryMainActivity.java
@@ -0,0 +1,8 @@
+package com.github.dipakkr.insidegu.HOGFeed;
+
+/**
+ * Created by deepak on 03-02-2017.
+ */
+
+public class StoryMainActivity {
+}
---
+++ app/src/main/java/com/github/dipakkr/insidegu/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.github.dipakkr.insidegu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by deepak on 03-02-2017.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(3000);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        ).start();
+    }
+
+}
